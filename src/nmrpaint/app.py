453a2c2d12d759766d3e69a3c1615c2c5e918f1c
PotatoCobbler <avdhchemistry@gmail.com>
2026-07-28 15:29:17 +0300

    Update app.py

--- src/nmrpaint/app.py
+++ src/nmrpaint/app.py
@@ -1150,8 +1150,6 @@ def _generate_local_pulse_program() -> Path:
         default="pulse_program",
     )
 
-    content = build_pulse_program_text()
-
     return save_text_local(
         content=content,
         filename=filename,
@@ -1209,10 +1207,13 @@ def prepare_browser_download(b):
         )
         print("filename =", repr(filename))
         
-        browser_download_link.value = build_text_download_link_html(
-            content=content,
-            filename=filename,
-            label=f"Download {filename}",
+        browser_download_link.value = (
+            f"<b>filename:</b> {filename}<br><br>" +
+            build_text_download_link_html(
+                content=content,
+                filename=filename,
+                label=f"Download {filename}",
+            )
         )
 
     except Exception as exc:
