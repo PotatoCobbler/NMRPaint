9213c090b200c3e6a3354af9227a5989b0f4c1a1
PotatoCobbler <avdhchemistry@gmail.com>
2026-07-28 19:13:58 +0300

    Update app.py

--- src/nmrpaint/app.py
+++ src/nmrpaint/app.py
@@ -2916,7 +2916,8 @@ ns_ds_row = HBox(
 
 exp_prop_row = HBox(
     [exp_title, exp_class, exp_dim, exp_2d_option, exp_type, exp_subtype, exp_incl, ns_text, ds_text, pulse_section, shape_section, exp_comment],
-    layout=Layout(spacing="1px")
+    layout=Layout(spacing="1px"),
+    padding='5px 0px'    
 )
 
 exp_prop_section = VBox([
