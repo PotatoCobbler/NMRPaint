10bed1a4afddd2cf91bfa3a3d932c8c937bbca47
PotatoCobbler <avdhchemistry@gmail.com>
2026-08-03 18:34:45 +0200

    Update app.py

--- src/nmrpaint/app.py
+++ src/nmrpaint/app.py
@@ -1525,7 +1525,7 @@ phase_cycle_header = HBox([
 
 phase_cycle_container = VBox([])
 phase_cycle_output = Textarea(
-    layout=Layout(width="406px", height="160px")
+    layout=Layout(width="400px", height="160px")
 )
 
 def add_phase_row(pulse, phase):
