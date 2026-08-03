e7e5cf804c0174bb71d078690b580aae99370946
PotatoCobbler <avdhchemistry@gmail.com>
2026-08-03 17:50:03 +0200

    Update app.py

--- src/nmrpaint/app.py
+++ src/nmrpaint/app.py
@@ -1525,7 +1525,7 @@ phase_cycle_header = HBox([
 
 phase_cycle_container = VBox([])
 phase_cycle_output = Textarea(
-    layout=Layout(width="436px", height="160px")
+    layout=Layout(width="416px", height="160px")
 )
 
 def add_phase_row(pulse, phase):
@@ -2121,7 +2121,7 @@ property_editor_box = VBox(
         update_button
     ],
     layout=Layout(
-        width="250px",
+        width="270px",
         overflow="hidden"
     )
 )
