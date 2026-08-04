023656b87cbf3e933984e8df04c568da044c013b
PotatoCobbler <avdhchemistry@gmail.com>
2026-08-04 14:20:17 +0300

    Update app.py

--- src/nmrpaint/app.py
+++ src/nmrpaint/app.py
@@ -1764,7 +1764,7 @@ pulse_program_box = VBox(
         pulse_program_output,
         phase_cycle_box
     ],
-    layout=layout(padding='5px 0px')
+    layout=Layout(padding='5px 0px')
 )
 
 pulse_program_box.layout.flex = "0 0 auto"
